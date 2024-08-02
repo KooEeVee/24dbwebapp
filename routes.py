ebbadc01b5d1bd7cf3c76b4c827823f9e32f42a3
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2024-08-02 19:00:53 +0300

    add templates register.html, login.html, logout.html

--- routes.py
+++ routes.py
@@ -4,3 +4,15 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
