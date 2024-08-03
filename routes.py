31ac52a9f4ad36f9933d4ddf08854c644bd84dd7
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2024-08-03 18:43:43 +0300

    add POST method to register and login forms

--- routes.py
+++ routes.py
@@ -1,17 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
-    return render_template("register.html")
+    if request.method=="GET":
+        return render_template("register.html")
+    else:
+        return redirect("/")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        return redirect("/")
 
 @app.route("/logout")
 def logout():
