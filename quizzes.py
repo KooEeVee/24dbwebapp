eed9c92ce66b0674936678e51ad41e7a401de8a0
Kaisa <45688258+KooEeVee@users.noreply.github.com>
2024-08-15 19:22:55 +0300

    show correct options on admin page

--- quizzes.py
+++ quizzes.py
@@ -190,7 +190,7 @@ def add_correctoption(optionid):
 
 def show_quizzes_toadmin(username):
     try:
-        sql = text("""SELECT quizzes.quiz_label, quizzes.published, quizzes.published_at, questions.quiz_id, questions.question_label, options.question_id, options.option_label 
+        sql = text("""SELECT quizzes.quiz_label, quizzes.published, quizzes.published_at, questions.quiz_id, questions.question_label, options.question_id, options.option_label, options.correct_option 
                    FROM quizzes 
                    LEFT JOIN questions ON quizzes.id=questions.quiz_id 
                    LEFT JOIN options ON questions.id=options.question_id 
@@ -220,39 +220,12 @@ def show_quizzes_toadmin(username):
                 }
             #print(dict_quizzes[quiz_label]["questions"][question_label])
             optionlist = {
-                "option_label": row.option_label
+                "option_label": row.option_label,
+                "correct_option": row.correct_option
             }
             dict_quizzes[quiz_label]["questions"][question_label]["options"].append(optionlist)
             #print(dict_quizzes)
         return dict_quizzes
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
-    
-def show_questions_options(quiz_id):
-    try:
-        sql = text("""SELECT questions.quiz_id, questions.question_label, options.question_id, options.option_label 
-                   FROM questions
-                   LEFT JOIN options ON questions.id=options.question_id 
-                   WHERE questions.quiz_id=:quiz_id""")
-        result = db.session.execute(sql, {"quiz_id":quiz_id})
-        list_questions = result.fetchall()
-        #print(list_quizzes)
-        dict_questions = {}
-        for row in list_questions:
-            question_label = row.question_label
-            #print(quiz_label)
-            if question_label not in dict_questions:
-                dict_questions[question_label] = {
-                    "question_id": row.question_id,
-                    "options": []
-                }
-            optionlist = {
-                "option_label": row.option_label
-            }
-            dict_questions[question_label]["options"].append(optionlist)
-            #print(dict_quizzes)
-        return dict_questions
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
